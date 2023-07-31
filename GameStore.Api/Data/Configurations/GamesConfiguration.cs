dae80b3f77684df6dbf1e4be005f19af422861d0
MagcDev <manuel117g@gmail.com>
2023-07-31 18:42:48 -0400

    Efcore

---
+++ GameStore.Api/Data/Configurations/GamesConfiguration.cs
@@ -0,0 +1,17 @@
+using GameStore.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameStore.Api.Data.Configurations;
+
+public class GamesConfiguration : IEntityTypeConfiguration<Game>
+{
+    public void Configure(EntityTypeBuilder<Game> builder)
+    {
+        builder.Property(game => game.Price)
+            .HasPrecision(5, 2);
+    }
+}
+
+
+    
